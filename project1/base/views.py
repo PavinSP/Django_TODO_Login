e95d8f9dcf10c88f8ed89690fa40f998fcb51519
PavinSP <pavinsp122002@gmail.com>
2024-12-26 11:41:38 +0530

    Update item

--- project1/base/views.py
+++ project1/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -18,6 +18,12 @@ class TaskDetail(DetailView):
     template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'base/task_form.html'
+
+class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
